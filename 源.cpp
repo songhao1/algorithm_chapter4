d8d1e787ba33d2dae5dc761188dcf94f67a11a8e
songhao1 <717977831@qq.com>
2020-07-16 01:16:59 +0800

    2020/7/16

--- 源.cpp
+++ 源.cpp
@@ -334,34 +334,83 @@
 //}
 
 
-//P115
-#include<cstdio>
-const int maxn = 11;
-//P为当前排列,hashTable记录整数x是否已经在P中
-int n, P[maxn], hashTable[maxn] = { false };
-//当前处理排列的第index号位
-void generateP(int index) {
-	if (index == n + 1) {
-		for (int i = 1; i <= n; i++) {
-			printf("%d", P[i]);
-		}
-		printf("\n");
-		return;
-	}
+////P115
+//#include<cstdio>
+//const int maxn = 11;
+////P为当前排列,hashTable记录整数x是否已经在P中
+//int n, P[maxn], hashTable[maxn] = { false };
+////当前处理排列的第index号位
+//void generateP(int index) {
+//	if (index == n + 1) {
+//		for (int i = 1; i <= n; i++) {
+//			printf("%d", P[i]);
+//		}
+//		printf("\n");
+//		return;
+//	}
+//
+//	for (int x = 1; x <= n; x++) {
+//		if (hashTable[x] == false) {
+//			P[index] = x;
+//			hashTable[x] = true;
+//			generateP(index + 1);
+//			hashTable[x] = false;
+//		}
+//	}
+//}
+//
+//int main() {
+//	n = 4;
+//	generateP(1);
+//
+//	return 0;
+//}
 
-	for (int x = 1; x <= n; x++) {
-		if (hashTable[x] == false) {
-			P[index] = x;
-			hashTable[x] = true;
-			generateP(index + 1);
-			hashTable[x] = false;
-		}
-	}
-}
 
-int main() {
-	n = 4;
-	generateP(1);
-
-	return 0;
-}
+////P118
+////PAT B1020
+//#include<cstdio>
+//#include<algorithm>
+//using namespace std;
+//
+//struct mooncake {
+//	double store;	//库存量
+//	double sell;	//总售价
+//	double price;	//单价
+//}cake[1010];
+//
+//bool cmp(mooncake a, mooncake b) {	//按单价从高到低排序
+//	return a.price > b.price;
+//}
+//
+//int main() {
+//	int n;
+//	double D;
+//	scanf("%d%lf", &n, &D);
+//
+//	for (int i = 0; i < n; i++) {
+//		scanf("%lf", &cake[i].store);
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		scanf("%lf", &cake[i].sell);
+//		cake[i].price = cake[i].sell / cake[i].store;	//计算单价
+//	}
+//
+//	sort(cake, cake + n, cmp);	//按单价从高到低排序
+//	double ans = 0;	//收益
+//	for (int i = 0; i < n; i++) {
+//		if (cake[i].store <= D) {	//如果需求量高于月饼库存量
+//			D -= cake[i].store;	//第i种月饼全部卖出
+//			ans += cake[i].sell;
+//		}
+//		else {	//如果月饼库存量高于需求量
+//			ans += cake[i].price * D;	//只卖出剩余需求量的月饼
+//			break;
+//		}
+//	}
+//
+//	printf("%.2f\n", ans);
+//
+//	return 0;
+//}
