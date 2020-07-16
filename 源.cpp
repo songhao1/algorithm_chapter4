aa32d4f0290000b586065f4a2e2a94e9e810dfc5
songhao1 <717977831@qq.com>
2020-07-17 02:04:45 +0800

    2020/7/17

--- 源.cpp
+++ 源.cpp
@@ -414,3 +414,98 @@
 //
 //	return 0;
 //}
+
+
+////P121
+////PAT B1023
+//#include<cstdio>
+//int main() {
+//	int count[10];	//记录数字0-9的个数
+//
+//	for (int i = 0; i < 10; i++) {
+//		scanf("%d", &count[i]);
+//	}
+//
+//	for (int i = 1; i < 10; i++) {	//从1-9中选择count不为0的最小的数字
+//		if (count[i] > 0) {
+//			printf("%d", i);
+//			count[i]--;
+//			break;	//找到一个之后就中断
+//		}
+//	}
+//
+//	for (int i = 0; i < 10; i++) {	//从0-9输出对应个数的数字
+//		for (int j = 0; j < count[i]; j++) {
+//			printf("%d", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+////P122
+//#include<cstdio>
+//#include<algorithm>
+//using namespace std;
+//
+//const int maxn = 110;
+//
+//struct Inteval {
+//	int x, y;	//开区间左右端点
+//}I[maxn];
+//
+//bool cmp(Inteval a, Inteval b) {
+//	if (a.x != b.x) return a.x > b.x;	//先按左端点从大到小排序
+//	else return a.y < b.y;	//左端点相同的按右端点从小到大排序
+//}
+//
+//int main() {
+//	int n;
+//	while (scanf("%d", &n), n != 0) {
+//		for (int i = 0; i < n; i++) {
+//			scanf("%d%d", &I[i].x, &I[i].y);
+//		}
+//
+//		sort(I, I + n, cmp);	//按区间排序
+//		//ans记录不相交区间个数,lastX记录上一个被选中区间的左端点
+//		int ans = 1, lastX = I[0].x;
+//		for (int i = 1; i < n; i++) {
+//			if (I[i].y <= lastX) {	//如果该区间右端点在lastX左边
+//				lastX = I[i].x;	//以I[i]作为新选中的区间
+//				ans++;	//不相交区间个数加1
+//			}
+//		}
+//		printf("%d\n", ans);
+//	}
+//	return 0;
+//}
+
+
+////P126
+//#include<cstdio>
+////A[]为严格递增序列,left为二分下界,right为二分上界,x为欲查询的数
+////二分区间为左闭右闭的[left,right],传入的初值为[0,n-1]
+//int binarySearch(int A[], int left, int right, int x) {
+//	int mid;	//mid为left和right的中点
+//	while (left <= right) {	//如果left>right就没办法形成闭区间了
+//		mid = (left + right) / 2;	//取中点
+//		if (A[mid] == x) return mid;	//找到x,返回下标
+//		else if (A[mid] > x) {	//中间的数大于x
+//			right = mid - 1;	//往左子区间[left,mid-1]查找
+//		}
+//		else {	//中间的数小于x
+//			left = mid + 1;	//往右子区间[mid+1,right]查找
+//		}
+//	}
+//
+//	return -1;	//查找失败,返回-1
+//}
+//
+//int main() {
+//	const int n = 10;
+//	int A[n] = { 1,3,4,6,7,8,10,11,12,15 };
+//	printf("%d %d\n", binarySearch(A, 0, n - 1, 6), binarySearch(A, 0, n - 1, 9));
+//
+//	return 0;
+//}
